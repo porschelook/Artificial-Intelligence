f76c0e67ebb3c1e4b394a00cd16977cb015b6840
Matthew Callahan <matthew.callahan@ieee.org>
2024-04-11 19:09:57 -0700

    add method to move robot

--- environment.py
+++ environment.py
@@ -7,6 +7,8 @@ import random
 NO_WALL = 0
 WALL = 1
 
+
+
 class room:
         Isclean = 0
         IsDoor = 1
@@ -16,7 +18,7 @@ class room:
          
        
 
-
+#define the room and the robot inside it. 
 class environment:
     
     rooms = None
@@ -29,24 +31,30 @@ class environment:
     current_x = 0
     current_y = 0
     Iswall = NO_WALL
-   
+    
+    UP,RIGHT,DOWN,LEFT=range(4)#define directions robot can face
+    INITIAL_DIRECTION=UP
     
     def __init__(self,wall):
         self.rooms = [[room(f"room {i}-{j}") for j in range(10)] for i in range(10)]
-         
+        UP,RIGHT,DOWN,LEFT=range(4)#define directions robot can face
         self.INITIAL_X = 0 #start point
         self.INITIAL_Y = 9 #start point
+        self.INITIAL_DIRECTION=UP
 
+        self.ROOM_DIMENSION=10
         self.Total_clean = 0
         self.Total_action = 0
 
         self.current_x = self.INITIAL_X #start point
         self.current_y = self.INITIAL_Y #start point
+        self.current_direction=self.INITIAL_DIRECTION #start direction
+        
         self.Iswall = wall
 
         if self.Iswall == WALL:
-            for i in range(0, 10):
-                for j in range(0, 10):
+            for i in range(0, self.ROOM_DIMENSION):
+                for j in range(0, self.ROOM_DIMENSION):
                     self.rooms[i][j].IsDoor  = 0 # set to wall
             #create Door set Door
             self.rooms[2][5].IsDoor = 1
@@ -54,7 +62,24 @@ class environment:
             self.rooms[7][4].IsDoor = 1
             self.rooms[5][7].IsDoor = 1
 
-
+    def advance(self):
+        #TODO implement movement based on orientation
+        if self.current_direction == environment.UP:
+                self.current_y-=1
+        if self.current_direction == environment.DOWN:
+                self.current_y+=1
+        if self.current_direction == environment.RIGHT:
+                self.current_x+=1
+        if self.current_direction == environment.LEFT:
+                self.current_x-=1
+        #fix position to be within bounds
+        self.current_y=max(0,self.current_y)
+        self.current_x=max(0,self.current_x)        
+        self.current_y=min(self.ROOM_DIMENSION,self.current_y)
+        self.current_x=min(self.ROOM_DIMENSION,self.current_x)        
+        #Next is to fix the robot from encountering a wall in the middle of the field
+                
+        
     def printCurrentWorld(self):
        
         
@@ -91,8 +116,5 @@ class environment:
                         print("  ", end="")
                 print("\n")
 
-            print("--------------------------------")
-
-            
-        
- 
+            print("--------------------------------")            
+    
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ model = int(input("Select Model: \n1 First_Model \n2 Second_Model \n3 Third_Mode
 match model:
     case 1:
         print("model_1")
+        
     case 2:
         print("model_2")
     case 3:
@@ -24,6 +25,7 @@ map = int(input("NoWall 1 \nWall 2\n"))
  
 if map == 1:
     state = environment( NO_WALL)
+    state.advance()
     state.printCurrentWorld()
 
  
