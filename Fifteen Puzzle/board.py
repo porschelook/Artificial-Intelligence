0164ca1565f214da0fa213d6cf7bd2817bcd555b
Matthew Callahan <matthew.callahan@ieee.org>
2024-04-26 13:46:59 -0700

    merge board classes and include scramble function

--- Fifteen Puzzle/board.py
+++ Fifteen Puzzle/board.py
@@ -2,14 +2,20 @@ import numpy as np
 import random
 
 class state:
-    def __init__(self):
+    def __init__(self, board=None):
         #init into the solved state
-        temp=np.arange(15)+1#fifteen for the fifteen puzzle.
-        temp=np.concatenate((temp,np.array([0])))#add in a zero for the empty cell
-        self.board=np.reshape(temp,(4,4))# this is in row-major order so the first index is which row it is in and the second is which coilumn it is in. 
+        if board is None:
+            temp=np.arange(15)+1#fifteen for the fifteen puzzle.
+            temp=np.concatenate((temp,np.array([0])))#add in a zero for the empty cell
+            self.board=np.reshape(temp,(4,4))# this is in row-major order so the first index is which row it is in and the second is which coilumn it is in.
+        else:
+            self.board=board.copy()#does a deep copy
+            
         self.emptyLoc=(3,3)#initialize the spot of the empty square so it doesn't need to be searched for
+        self.row=4
+        self.col=4
 
-    def scramble(numSteps):
+    def scramble(self,numSteps):
         lastMove=None
         moves={"up", "right","left","down"}
         for i in range(numSteps):
@@ -17,6 +23,14 @@ class state:
             #randomly choose a step that isn't the last one
             lastMove=move
             #apply move
+            if move=="up":
+                self.moveUp()
+            if move=="right":
+                self.moveRight()
+            if move=="left":
+                self.moveLeft()
+            if move=="down":
+                self.moveDown()    
     #changes the board state to that for which the empty (0) square has moved up one tile.         
     def moveUp(self):
         assert self.board[self.emptyLoc]==0
@@ -43,3 +57,19 @@ class state:
         self.board[newEmptyLoc], self.board[self.emptyLoc]=self.board[self.emptyLoc], self.board[newEmptyLoc]
         self.emptyLoc=newEmptyLoc
 
+    def manh_dist(self):
+        dist = 0
+        for row in range(self.row):
+            for col in range(self.col):
+
+                if (value := self.board[row,col]) != 0:
+                    print("v ",value)
+                    value -= 1
+                    x = value % self.col
+                    y = value // self.row
+                    dist += abs(x - col) + abs(y - row)
+                    print("dist ",dist)
+
+        return dist
+    def copy(self):
+        return state(board=self.board)
