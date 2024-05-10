7f8e876c29b7ac0deb490111eb558574d15e1acd
porschelook <33505600+porschelook@users.noreply.github.com>
2024-05-09 23:54:55 -0700

    board

--- SuDoKu/board.py
+++ SuDoKu/board.py
@@ -1,38 +1,45 @@
 class board:
     def __init__(self):
-        self.cells=[[{1 ,2,3,4,5,6,7,8,9} for col in range(9)] for row in range(9)]
+        self.cells = [[{1, 2, 3, 4, 5, 6, 7, 8, 9} for col in range(9)] for row in range(9)]
 
-    def fillCell(self, row,col,value):
+    def fillCell(self, row, col, value):
         if value not in self.cells[row][col]:
-            print("attempt at invalid move")
-            return#decide how to handles this case later
-        
-        self.cells[row][col]={value} #make the assignment
-        #eliminate the value from other applicapble cells
+            print("Attempt at invalid move")
+            return  # decide how to handle this case later
 
-        #eliminate from each element in same row
+        self.cells[row][col] = {value}  # make the assignment
+
+        # eliminate the value from other applicable cells
+
+        # eliminate from each element in same row
         for itercol in range(9):
             if itercol == col:
                 continue
-            self.cells[row][itercol].discard(value )
+            self.cells[row][itercol].discard(value)
 
-        #eliminate from each element in same column
+        # eliminate from each element in same column
         for iterrow in range(9):
             if iterrow == row:
                 continue
             self.cells[iterrow][col].discard(value)
 
-        #eliminate the value from each element in the box
-        rowoffset=row//3
-        coloffset=col//3
+        # eliminate the value from each element in the box
+        rowoffset = row // 3
+        coloffset = col // 3
         for rowiter in range(3):
             for coliter in range(3):
-                self.cells[rowoffset+rowiter][coloffset+coliter].discard(value)
-    def printBoard(self):
-        for i in range(9):
-            print(self.cells[i]," ",end="")
-            print()
-             
- 
+                self.cells[rowoffset * 3 + rowiter][coloffset * 3 + coliter].discard(value)
 
-    
+    def printBoard(self):
+        for row in range(9):
+            if row % 3 == 0 and row != 0:
+                print("-" * 21)
+            for col in range(9):
+                if col % 3 == 0 and col != 0:
+                    print("|", end=" ")
+                value = self.cells[row][col]
+                if len(value) == 1:
+                    print(list(value)[0], end=" ")
+                else:
+                    print(".", end=" ")
+            print()
