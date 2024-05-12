aa89868534073159a9f10816723e57f8a4e35405
Matthew Callahan <matthew.callahan@ieee.org>
2024-05-12 14:13:26 -0700

    add first search code. Currently broken

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,6 +1,29 @@
 from board import *
 import time
 
+def recursiveSimple(board):
+    if board.toFill==0:
+        return board
+    if not board.forwardCheck():
+        return None
+    #now select an index 
+    fillX,fillY=board.emptyCells[0]
+    #prepare for backtracking
+    backup=board.copy()
+    moveDomain=backup.cells[fillX][fillY].copy()
+    print(moveDomain)
+    print(str(fillX) + ", "+str(fillY))
+    backup.printBoard()
+    for move in moveDomain:
+        board.fillCell(fillX,fillY,move)
+        solution=recursiveSimple(board)
+        if solution is None:
+            board=backup.copy()
+        elif len(solution.emptyCells)==0:
+            return solution
+        else:
+            solution.printBoard()
+    return None
 if __name__ == "__main__":
 
     b = board()
@@ -26,6 +49,10 @@ if __name__ == "__main__":
     # for x in all_board:
       #  print(x)
     
-    s = open("../code_1/Sudoku/testExample.txt", "r")
-    b.buildBoard("../code_1/Sudoku/testExample.txt")
+    #s = open("../code_1/Sudoku/testExample.txt", "r")
+    b.buildBoard("testExample.txt")
     b.printBoard()
+    print(b.emptyCells)
+    print(b.toFill)
+    solvedBoard=recursiveSimple(b)
+    solvedBoard.printBoard()
