5845d0736d3980736460047bed87f2023579066c
Matthew Callahan <matthew.callahan@ieee.org>
2024-06-12 21:37:49 -0700

    scan first pass

--- Final/NewAgent.py
+++ Final/NewAgent.py
@@ -1,45 +1,54 @@
 from environment import *
 import itertools
+import copy
 Up, Down, Left, Right= range(4)
 
 class BeliefNode:
-    def __init__(self,env):
+    def __init__(self,env=None):
+        if env==None:
+            
+            #extract list of walls, build new architecture that captures belief states.
+            #for now there are no walls
+            self.beliefCells=[]
+            self.current_x=0
+            self.current_y=0
+            self.currentFacing=Up
+            #for now, use fixed size
+            self.scanDistance=2
+            self.height=0
+            self.width=0
+            #need to run an and-or plan
+            return
+        
         #extract list of walls, build new architecture that captures belief states.
         #for now there are no walls
         self.beliefCells=[[{0,1} for cell in row] for row in env.rooms]
         self.current_x=env.current_x
         self.current_y=env.current_y
         self.currentFacing=Up
-        
+        self.height=len(self.beliefCells)
+        self.width=len(self.beliefCells[1])
         self.scanDistance=env.scanDistance
         #for now, use fixed size
         self.scanDistance=2
-    def __init__(self):
-        #extract list of walls, build new architecture that captures belief states.
-        #for now there are no walls
-        self.beliefCells=[]
-        self.current_x=0
-        self.current_y=0
-        self.currentFacing=Up
-        
-        #for now, use fixed size
-        self.scanDistance=2
-        #need to run an and-or plan
     #return a deep copy
     def copy(self):
         out=BeliefNode()
-        out.beliefCells=self.beliefCells.copy()
+        out.beliefCells=copy.deepcopy(self.beliefCells)
         out.current_x=self.current_x
         out.current_y=self.current_y
         out.currentFacing= self.currentFacing
         out.scanDistance=self.scanDistance
+        out.height=len(self.beliefCells)
+        out.width=len(self.beliefCells[1])
+        return out
         
                
     #This will return a set of states based on all posibilities of the scanning operation
     def scan(self):
-       
+        #TODO remove randomization from states already known
         out=[]
-        if self.currentFacine==Up:
+        if self.currentFacing==Up:
             selectx=self.current_x
             starty=self.current_y
             endy=max(0,self.current_y-self.scanDistance)
@@ -48,11 +57,60 @@ class BeliefNode:
                 additionalElement=self.copy()
                 j=0
                 for y in range(starty,endy, -1):
-                    additionalElement.beliefCells[y][selectx]=item[j]
+                    #print(item[j])
+                    #print(additionalElement.beliefCells[y][selectx])
+                    if (additionalElement.beliefCells[y][selectx])=={1,0}:
+                        additionalElement.beliefCells[y][selectx]=item[j]
+                        
                     j+=1
-                out.append(additionalItem)
+                out.append(additionalElement)
             return out
-
+        
+        if self.currentFacing==Down:
+            selectx=self.current_x
+            starty=self.current_y
+            endy=min(self.height,self.current_y+self.scanDistance)
+            perms=itertools.product([0,1],repeat=starty-endy)
+            for item in perms:
+                additionalElement=self.copy()
+                j=0
+                for y in range(starty,endy, 1):
+                    if (additionalElement.beliefCells[y][selectx])=={1,0}:
+                        additionalElement.beliefCells[y][selectx]=item[j]
+                    j+=1
+                out.append(additionalElement)
+            return out
+        
+        if self.currentFacing==Left:
+            selecty=self.current_y
+            startx=self.current_x
+            endx=max(0,self.current_x-self.scanDistance)
+            perms=itertools.product([0,1],repeat=startx-endx)
+            for item in perms:
+                additionalElement=self.copy()
+                j=0
+                for x in range(starty,endy, -1):
+                    if (additionalElement.beliefCells[selecty][x])=={1,0}:
+                        additionalElement.beliefCells[selecty][x]=item[j]
+                    j+=1
+                out.append(additionalElement)
+            return out
+        
+        if self.currentFacing==Right:
+            selecty=self.current_y
+            startx=self.current_x
+            endx=min(self.width,self.current_x+self.scanDistance)
+            perms=itertools.product([0,1],repeat=startx-endx)
+            for item in perms:
+                additionalElement=self.copy()
+                j=0
+                for x in range(starty,endy, 1):
+                    if (additionalElement.beliefCells[selecty][x])=={1,0}:
+                        additionalElement.beliefCells[selecty][x]=item[j]
+                    j+=1
+                out.append(additionalElement)
+            return out
+           
 class NewAgent(Agent):
     def __init__(self, env):# needs to generate the plan before it executes it
         self.beliefSpace=[BeliefNode(env)]
